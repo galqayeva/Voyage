ebf541e66de1de12d52f48c4b24cee68e495191c
galqayeva <galqayeva@gmail.com>
2017-11-06 15:31:44 +0400

    filtering

--- app/src/main/java/g/y/v/vyg/FilteringActivity.java
+++ app/src/main/java/g/y/v/vyg/FilteringActivity.java
@@ -2,12 +2,158 @@ package g.y.v.vyg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.database.Cursor;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FilteringActivity extends AppCompatActivity {
+import g.y.v.vyg.Utils.DatabaseHelper;
+import g.y.v.vyg.Utils.GPSTracker;
+import g.y.v.vyg.Utils.Model;
+import g.y.v.vyg.Utils.MyAdapter;
+import g.y.v.vyg.Utils.MySingleTon;
+
+
+public class FilteringActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private GPSTracker gpsTracker;
+    private Location mLocation;
+    double latitude, longitude;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<Model> modelList;
+    private ProgressDialog progressDialog;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    DatabaseHelper myDB;
+    String url,Tag="salus";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtering);
+
+
+        swipeRefreshLayout=(SwipeRefreshLayout)findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(FilteringActivity.this);
+
+        recyclerView=(RecyclerView)findViewById(R.id.recycleview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        myDB = new DatabaseHelper(getApplicationContext());
+        modelList=new ArrayList<>();
+
+        gpsTracker = new GPSTracker(getApplicationContext());
+        latitude = gpsTracker.getLocation().getLatitude();
+        longitude = gpsTracker.getLocation().getLongitude();
+
+        url= Constants.gmApi(Double.toString(latitude),Double.toString(longitude),"500");
+        loadListview();
+        loadRestaurants();
+    }
+
+
+    public void loadRestaurants(){
+
+        Cursor data = myDB.getAlldata();
+
+        if(data.getCount() == 0)
+        {
+            progressDialog = new ProgressDialog(getApplicationContext());
+            progressDialog.setMessage("Getting Restaurants");
+            progressDialog.show();
+
+            StringRequest stringRequest=new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            try {
+                                JSONObject jsonObject=new JSONObject(response);
+                                JSONArray jsonArrayResult=jsonObject.getJSONArray("results");
+
+                                for (int i=0;i<jsonArrayResult.length();i++)
+                                {
+                                    JSONObject jsonobject=jsonArrayResult.getJSONObject(i);
+                                    String lat = jsonobject.getJSONObject("geometry").getJSONObject("location").getString("lat");
+                                    String lng = jsonobject.getJSONObject("geometry").getJSONObject("location").getString("lng");
+                                    String restaurantName=jsonobject.getString("name");
+
+                                    boolean insertData = myDB.addData(restaurantName,lng,lat);
+                                    if(!insertData==true)
+                                        Log.d(Tag,"Inserting problem");
+                                }
+
+                                modelList.clear();
+                                loadListview();
+                                progressDialog.dismiss();
+
+                            } catch (JSONException e) {
+
+                                Log.d(Tag, "json parse error");
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                            Log.d(Tag,"String request Response error ----"+ error.getMessage());
+                        }
+                    }
+
+            );
+            MySingleTon.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+
+            if(swipeRefreshLayout.isRefreshing())
+            {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        }
+    }
+
+
+    public void loadListview(){
+
+        Cursor data = myDB.getAlldata();
+
+        if(data.getCount() != 0)
+        {
+            Log.d(Tag,"----------"+data.getCount());
+            while(data.moveToNext())
+            {
+                Model item=new Model(data.getString(1),data.getString(2),data.getString(3));
+                modelList.add(item);
+            }
+
+            adapter=new MyAdapter(modelList,getApplicationContext());
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+
+    @Override
+    public void onRefresh() {
+        myDB.deleteAll();
+        loadRestaurants();
     }
 }
--- app/src/main/java/g/y/v/vyg/Utils/MyAdapter.java
+++ app/src/main/java/g/y/v/vyg/Utils/MyAdapter.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import g.y.v.vyg.R;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
