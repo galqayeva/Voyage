0129f50a3acd7ca2b1c90c694463e948cfe9ae32
galqayeva <galqayeva@gmail.com>
2017-11-08 17:39:28 +0400

    loginwith fb

--- app/src/main/java/g/y/v/vyg/Activities/LoginActivity.java
+++ app/src/main/java/g/y/v/vyg/Activities/LoginActivity.java
@@ -1,21 +1,85 @@
 package g.y.v.vyg.Activities;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import g.y.v.vyg.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btn;
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions("email","public_profile");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                String user_id=loginResult.getAccessToken().getUserId();
+
+                GraphRequest graphRequest=GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+
+                        displayUserInfo(object);
+                    }
+                });
+
+                Bundle parameters=new Bundle();
+                parameters.putString("fields","first_name, last_name, email, id");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
 
         btn=(Button)findViewById(R.id.button3);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +89,25 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+
+     public void displayUserInfo(JSONObject jsonObject){
+
+         String firstname="",lastname="",email="",id="";
+         try {
+             firstname=jsonObject.getString("first_name");
+             lastname=jsonObject.getString("last_name");
+             email=jsonObject.getString("email");
+             id=jsonObject.getString("id");
+
+         } catch (JSONException e) {
+             e.printStackTrace();
+         }
+
+         TextView tv=(TextView)findViewById(R.id.fb);
+         tv.setText(firstname+"--"+lastname+"--"+email+"--"+id);
+
+     }
 
 }
